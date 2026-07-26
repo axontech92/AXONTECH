8e78b10131fe4d416887b609269fc1d2f62a17ef
axontech92 <axontech92@gmail.com>
2026-07-26 13:52:34 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'axontech-v70';
+const CACHE = 'axontech-v71';
 const STATIC = [
   './', './index.html', './admin.html', './app.css', './app.js', 
   './manifest.json', './productos.json', './categorias.json',
